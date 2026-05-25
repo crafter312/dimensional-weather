cb1e15b622fc2011c59e13e278fb5969fe8179fb
crafter312 <henrysquirewebb@gmail.com>
2026-05-25 08:06:21 -0500

    set up dual fabric/forge development environment

--- fabric/src/main/java/net/thesquire/dimensionalweather/client/DimensionalWeatherClient.java
+++ fabric/src/main/java/net/thesquire/dimensionalweather/client/DimensionalWeatherClient.java
@@ -2,7 +2,7 @@ package net.thesquire.dimensionalweather.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class DimensionalweatherClient implements ClientModInitializer {
+public class DimensionalWeatherClient implements ClientModInitializer {
     /**
      * Runs the mod initializer on the client environment.
      */
